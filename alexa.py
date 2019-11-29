f2fce75833d7715d155cee4744535cc6269d24a3
Neil Stewart <neil.stewart@tescobank.com>
2019-11-29 10:41:33 +0000

    init commit

---
+++ alexa.py
@@ -0,0 +1,36 @@
+import json
+
+
+def get_instances(e, c):
+    print("hello logs")
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            "message": "yo papa"
+        })
+    }
+
+
+def start(event, context):
+
+    print(event)
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
