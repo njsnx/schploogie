8b3456ab6b35458c22106b85e36b91e485d2e43b
Gordon Thomson <B021442@FVFXF4Q0HV2D.local>
2019-11-29 13:10:30 +0000

    schloogie doo, where are you?

--- functions.py
+++ functions.py
@@ -4,7 +4,47 @@ import boto3
 def get_instances(e,c):
     client = boto3.client('ec2')
     results = client.describe_instances()
-    print(results)
+    # print(results)
+    processed_instances = []
+
+    for reservations in results['Reservations']:
+        # print(reservations)
+        for instance in reservations['Instances']:
+            processed_instance = {}
+
+            processed_instance['instance_id'] = instance['InstanceId'] 
+            processed_instance['instance_state'] = instance['State']['Name']
+            
+            for tags in instance['Tags']:
+                if tags["Key"] == 'Name':
+                    # print(tags["Value"])
+                    processed_instance['name'] = tags['Value']
+            processed_instances.append(processed_instance)
+    
+    response = {
+        'statusCode': 200,
+        'body': {
+            "instances": json.dumps(
+            processed_instances
+        )
+        }
+    }
+
+    print(json.dumps(response))
+    return response
+
+    #
+    # {
+    #     body: {
+    #         "instances": [
+    #             {
+    #                 "instance-id":12312312
+    #                 "instance-name":adasdsad
+    #                 "state": dasdada
+    #             }
+    #         ]
+    #     }
+    # }
 
 def get_health_events(e, c):
     client = boto3.client('health')
